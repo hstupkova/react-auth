ab5a43f77b1876b9fae1d1dac472b361d653656e
Hana Stupková <hstupkova@gmail.com>
2021-11-17 15:46:28 +0100

    storing token in local storage

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,16 +8,21 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  // this will be either undefined or the token string
+  const initialToken = localStorage.getItem('token');
+  // this is possible because localStorage is a synchronous API
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem('token', token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem('token');
   };
 
   const contextValue = {
